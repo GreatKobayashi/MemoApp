ec8a8424d32d162e8d7c9d254ea6996de5cb906b
DESKTOP-V8DLB94\kobay <kobayashiryu123@gmail.com>
2025-03-09 20:23:34 +0900

    [#15]テスト画面映るように

---
+++ src/MemoApp.UI/App.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoApp.UI
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ src/MemoApp.UI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MemoApp.UI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
